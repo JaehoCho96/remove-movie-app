e5cb627c1fb047e1c6139577bdb228dde31a70ee
JaehoCho96 <theker243@naver.com>
2023-08-08 16:53:00 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy"
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'Tori / ChoJaeHo',
+  email: 'jaeho895@gmail.com',
+  blog: '',
+  github: 'https://github.com/JaehoCho96',
+  repository: 'https://github.com/JaehoCho96/vanillajs-movie-app'
+})
